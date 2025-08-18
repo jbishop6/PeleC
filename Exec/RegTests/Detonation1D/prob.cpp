f7a4bfb0595ea6941e26b2e642d770495b5127af
Jennifer Bishop <jenniferjbshp@gmail.com>
2025-08-18 08:32:17 -0600

    Update prob.cpp

--- Exec/RegTests/Detonation1D/prob.cpp
+++ Exec/RegTests/Detonation1D/prob.cpp
@@ -1,8 +1,10 @@
+struct ProbParm;
+
 #include <AMReX.H>
 #include <PeleC.H>
 #include <IndexDefines.H>
 #include <EOS.H>
-#include "prob_parm.H"   // <-- ensure ProbParm is declared
+// #include "prob_parm.H"   // <-- ensure ProbParm is declared
 #include "prob.H"
 
 using namespace amrex;
