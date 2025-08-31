c82c41e9cdd28cfc7e49b57b9e2dd644add9a2a0
Jennifer Bishop <jenniferjbshp@gmail.com>
2025-08-30 21:28:13 -0600

    Update prob.cpp

--- Exec/RegTests/Detonation1D/prob.cpp
+++ Exec/RegTests/Detonation1D/prob.cpp
@@ -1,4 +1,6 @@
 #include "prob.H"
+#include <AMReX_ParmParse.H>
+#include <vector>
 
 void
 pc_prob_close()
@@ -28,59 +30,48 @@ amrex_probinit(
     pp.query("frac", PeleC::h_prob_parm_device->frac);
     pp.query("idir", PeleC::h_prob_parm_device->idir);
     pp.query("use_Tinit", PeleC::h_prob_parm_device->use_Tinit);
+
+    std::string leftGas  = "N2", rightGas = "HE";
+    pp.query("left_gas",  leftGas);
+    pp.query("right_gas", rightGas);
+    auto eos = pele::physics::PhysicsType::eos();
+    PeleC::h_prob_parm_device->left_gas_id  = eos.speciesIndex(leftGas);
+    PeleC::h_prob_parm_device->right_gas_id = eos.speciesIndex(rightGas);
   }
-  for (int idir = 0; idir < AMREX_SPACEDIM; idir++) {
+  
+  for (int idir = 0; idir < AMREX_SPACEDIM; ++idir) {
+    const amrex::Real lo = problo[idir];
+    const amrex::Real hi = probhi[idir];
     PeleC::h_prob_parm_device->split[idir] =
-      PeleC::h_prob_parm_device->frac * (problo[idir] + probhi[idir]);
+        lo + PeleC::h_prob_parm_device->frac * (hi - lo);
   }
 
-  amrex::Real e_l;
-  amrex::Real e_r /*, cs, cp */;
-  amrex::Real massfrac[NUM_SPECIES] = {0.0};
-  massfrac[0] = 1.0;
-
+  amrex::Real e_l, e_r;
   auto eos = pele::physics::PhysicsType::eos();
+  std::vector<amrex::Real> Y(eos.numSpecies(), 0.0);
+
+   // Left state
+  std::fill(Y.begin(), Y.end(), 0.0);
+  if (PeleC::h_prob_parm_device->left_gas_id >= 0)
+    Y[PeleC::h_prob_parm_device->left_gas_id] = 1.0;
   if (PeleC::h_prob_parm_device->use_Tinit) {
-    eos.RTY2P(
-      PeleC::h_prob_parm_device->rho_l, PeleC::h_prob_parm_device->T_l,
-      massfrac, PeleC::h_prob_parm_device->p_l);
-    eos.RYP2E(
-      PeleC::h_prob_parm_device->rho_l, massfrac,
-      PeleC::h_prob_parm_device->p_l, e_l);
-    PeleC::h_prob_parm_device->rhoe_l = PeleC::h_prob_parm_device->rho_l * e_l;
-    eos.RTY2P(
-      PeleC::h_prob_parm_device->rho_r, PeleC::h_prob_parm_device->T_r,
-      massfrac, PeleC::h_prob_parm_device->p_r);
-    eos.RYP2E(
-      PeleC::h_prob_parm_device->rho_r, massfrac,
-      PeleC::h_prob_parm_device->p_r, e_r);
-    PeleC::h_prob_parm_device->rhoe_r = PeleC::h_prob_parm_device->rho_r * e_r;
-  } else {
-    eos.RYP2E(
-      PeleC::h_prob_parm_device->rho_l, massfrac,
-      PeleC::h_prob_parm_device->p_l, e_l);
-    eos.EY2T(e_l, massfrac, PeleC::h_prob_parm_device->T_l);
-    PeleC::h_prob_parm_device->rhoe_l = PeleC::h_prob_parm_device->rho_l * e_l;
-    eos.RYP2E(
-      PeleC::h_prob_parm_device->rho_r, massfrac,
-      PeleC::h_prob_parm_device->p_r, e_r);
-    eos.EY2T(e_r, massfrac, PeleC::h_prob_parm_device->T_r);
-    PeleC::h_prob_parm_device->rhoe_r = PeleC::h_prob_parm_device->rho_r * e_r;
+    eos.RTY2P(PeleC::h_prob_parm_device->rho_l, PeleC::h_prob_parm_device->T_l,
+              Y.data(), PeleC::h_prob_parm_device->p_l);
   }
-}
-}
+  eos.RYP2E(PeleC::h_prob_parm_device->rho_l, Y.data(),
+            PeleC::h_prob_parm_device->p_l, e_l);
+  PeleC::h_prob_parm_device->rhoe_l = PeleC::h_prob_parm_device->rho_l * e_l;
 
-void
-PeleC::problem_post_timestep()
-{
+  // Right state
+  std::fill(Y.begin(), Y.end(), 0.0);
+  if (PeleC::h_prob_parm_device->right_gas_id >= 0)
+    Y[PeleC::h_prob_parm_device->right_gas_id] = 1.0;
+  if (PeleC::h_prob_parm_device->use_Tinit) {
+    eos.RTY2P(PeleC::h_prob_parm_device->rho_r, PeleC::h_prob_parm_device->T_r,
+              Y.data(), PeleC::h_prob_parm_device->p_r);
+  }
+  eos.RYP2E(PeleC::h_prob_parm_device->rho_r, Y.data(),
+            PeleC::h_prob_parm_device->p_r, e_r);
+  PeleC::h_prob_parm_device->rhoe_r = PeleC::h_prob_parm_device->rho_r * e_r;
 }
-
-void
-PeleC::problem_post_init()
-{
-}
-
-void
-PeleC::problem_post_restart()
-{
-}
+} 
