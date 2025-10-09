88deda0585fa92a83e4fbcdacb90351b1f5c7443
Jennifer Bishop <jenniferjbshp@gmail.com>
2025-10-09 13:42:47 -0600

    Update eb_geometry.cpp

--- Exec/RegTests/Detonation2D/eb_geometry.cpp
+++ Exec/RegTests/Detonation2D/eb_geometry.cpp
@@ -1,8 +1,6 @@
 #include <AMReX_EB2.H>
 #include <AMReX_EB2_IF_Box.H>
-#include <AMReX_EB2_GeometryShop.H>
 #include <AMReX_ParmParse.H>
-#include <AMReX_REAL.H>
 
 using namespace amrex;
 
@@ -10,23 +8,18 @@ EB2::GeometryShop<EB2::BoxIF> makeGeometry()
 {
     ParmParse pp("eb2");
 
+    // Use std::vector to hold raw input first
+    std::vector<Real> lo_vec, hi_vec;
+    pp.getarr("box_lo", lo_vec, 0, AMREX_SPACEDIM);
+    pp.getarr("box_hi", hi_vec, 0, AMREX_SPACEDIM);
+
+    // Copy to RealArray
     RealArray lo, hi;
-    pp.getarr("box_lo", lo, 0, AMREX_SPACEDIM);
-    pp.getarr("box_hi", hi, 0, AMREX_SPACEDIM);
-    int has_fluid_inside = 1;
-    pp.query("box_has_fluid_inside", has_fluid_inside);
+    for (int i = 0; i < AMREX_SPACEDIM; ++i) {
+        lo[i] = lo_vec[i];
+        hi[i] = hi_vec[i];
+    }
 
-    EB2::BoxIF box(lo, hi, static_cast<bool>(has_fluid_inside));
+    EB2::BoxIF box(lo, hi, false);  // false = fluid outside
     return EB2::makeShop(box);
-}
-
-
-void setupEBGeometry(const Geometry& geom, int required_level, int max_level)
-{
-    EB2::Initialize();
-
-    auto shop = makeGeometry();  // Now valid!
-    EB2::Build(shop, geom, required_level, max_level);
-
-    amrex::Print() << "[EB] setupEBGeometry: Initialize + Build\n";
 }
