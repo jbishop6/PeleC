16c3973000f68cbd8cabc64f2a582bc78a375259
Jennifer Bishop <jenniferjbshp@gmail.com>
2025-10-10 09:58:54 -0600

    Update eb_geometry.cpp

--- Exec/RegTests/Detonation2D/eb_geometry.cpp
+++ Exec/RegTests/Detonation2D/eb_geometry.cpp
@@ -1,39 +1,32 @@
+#include <AMReX.H>
 #include <AMReX_EB2.H>
 #include <AMReX_EB2_IF_Box.H>
 #include <AMReX_EB2_GeometryShop.H>
-#include <AMReX_ParmParse.H>
-#include <AMReX_EB2_Register.H>
-#include "TwoBranch.H"  // <- Your geometry class header
-
-AMREX_EB2_DECLARE_GEOMETRY(TwoBranch, twobranch)
 
 using namespace amrex;
 
 EB2::GeometryShop<EB2::BoxIF> makeGeometry()
 {
-    RealArray lo = {0.4, 0.03};  // lower corner of box
-    RealArray hi = {0.6, 0.09};  // upper corner of box
-    bool has_fluid_inside = false;
+    ParmParse pp("eb2");
+    RealArray lo, hi;
+    pp.getarr("box_lo", lo, 0, AMREX_SPACEDIM);
+    pp.getarr("box_hi", hi, 0, AMREX_SPACEDIM);
 
-    // Print for debug
-    amrex::Print() << "[EB] Drawing BoxIF from " << lo[0] << "," << lo[1]
-                   << " to " << hi[0] << "," << hi[1] << "\n";
+    const bool has_fluid_inside = false;
+    EB2::BoxIF box({lo[0], lo[1]}, {hi[0], hi[1]}, has_fluid_inside);
 
-    EB2::BoxIF box(lo, hi, has_fluid_inside);
-    EB2::GeometryShop<EB2::BoxIF> shop(box);
-    return shop;
+    amrex::Print() << "[EB] Drawing BoxIF from "
+                   << lo[0] << "," << lo[1] << " to " << hi[0] << "," << hi[1] << "\n";
+
+    return EB2::GeometryShop<EB2::BoxIF>(box);
 }
 
-void setupEBGeometry(const amrex::Geometry& geom, int required_level, int max_level)
+void setupEBGeometry(const Geometry& geom, int required_level, int max_level)
 {
-    amrex::Print() << "[EB] setupEBGeometry: Initialize + Build\n";
+    EB2::Initialize();  // Only needed once per run
 
-    // Initialize EB2
-    amrex::EB2::Initialize();
-
-    // This builds EB based on what is in the input file (eb2.geom_type, etc.)
-    amrex::EB2::Build(geom, required_level, max_level);
+    auto shop = EB2::makeShop(makeGeometry());
+    EB2::Build(shop, geom, required_level, max_level);
 
     amrex::Print() << "[EB] Geometry built successfully.\n";
 }
-
