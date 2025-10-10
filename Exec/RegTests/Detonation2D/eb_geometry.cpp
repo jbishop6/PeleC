cf4d4af55e5a45705e44380fb2d89224469b86d9
Jennifer Bishop <jenniferjbshp@gmail.com>
2025-10-10 17:20:53 -0600

    Update eb_geometry.cpp

--- Exec/RegTests/Detonation2D/eb_geometry.cpp
+++ Exec/RegTests/Detonation2D/eb_geometry.cpp
@@ -23,4 +23,4 @@ void setupEBGeometry(const Geometry& geom, int required_level, int max_level)
     amrex::Print() << "[EB] Geometry built successfully.\n";
 }
 
-REGISTER_EB2_SHAPE(TwoBranchIF, "twobranch")
+//REGISTER_EB2_SHAPE(TwoBranchIF, "twobranch")
