01924f44320c5ad217e5d52e4456dbd5b32fb198
Jennifer Bishop <jenniferjbshp@gmail.com>
2025-11-10 13:30:37 -0700

    Update run_simulation.sh

--- Exec/RegTests/Detonation2D/run_simulation.sh
+++ Exec/RegTests/Detonation2D/run_simulation.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=geom_opt         # Name of the job
-#SBATCH --output=output.log         # Where to write the output
-#SBATCH --error=error.log           # Where to write errors
-#SBATCH --time=01:00:00             # Max run time (hh:mm:ss)
-#SBATCH --partition=standard        # Partition/queue to submit to (may vary)
-#SBATCH --ntasks=1                  # Number of tasks (usually 1 for a Python script)
-#SBATCH --cpus-per-task=4           # CPUs per task
-#SBATCH --mem=4G                    # Memory per node
+#SBATCH --job-name=opt_geo
+#SBATCH --output=slurm_output.log
+#SBATCH --error=slurm_error.log
+#SBATCH --partition=compute-long
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
 
 echo "Job started on $(hostname) at $(date)"
 
