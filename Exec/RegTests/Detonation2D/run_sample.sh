912fc7b8dec71f9ef8a5d4faa2bcf84bb943479f
Jennifer Bishop <jenniferjbshp@gmail.com>
2025-11-17 22:23:51 -0700

    Update run_sample.sh

--- Exec/RegTests/Detonation2D/run_sample.sh
+++ Exec/RegTests/Detonation2D/run_sample.sh
@@ -5,14 +5,17 @@
 #SBATCH --partition=compute-long
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
-#SBATCH --time=02:00:00
 
 echo "Started job on $(hostname) at $(date)"
 
+# === Load Conda ===
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate pelecopt
+
+# === Go to working directory ===
 cd "$SLURM_SUBMIT_DIR"
 
+# === Run sample ===
 SAMPLE_FILE=$1
 python run_sample_from_file.py "$SAMPLE_FILE"
 
