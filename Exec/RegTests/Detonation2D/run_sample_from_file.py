9d983652950512b63b8f78688137264b84903841
Jennifer Bishop <jenniferjbshp@gmail.com>
2025-11-17 22:30:24 -0700

    Update run_sample_from_file.py

--- Exec/RegTests/Detonation2D/run_sample_from_file.py
+++ Exec/RegTests/Detonation2D/run_sample_from_file.py
@@ -2,10 +2,8 @@ import sys
 import numpy as np
 from optimize_geometry import run_pelec_and_extract_thrust
 
-sample_path = sys.argv[1]
-Z, X, H, W = np.loadtxt(sample_path)
+sample_file = sys.argv[1]
+sample = np.loadtxt(sample_file)
 
-INP_FILE = "inputs.detonation.threebranch.inp"
-EXECUTABLE = "./PeleC2d.gnu.ex"
-
-run_pelec_and_extract_thrust(Z, X, H, W, INP_FILE, EXECUTABLE)
+geo_Z, geo_X, geo_H, geo_W = sample
+run_pelec_and_extract_thrust(geo_Z, geo_X, geo_H, geo_W, "inputs.detonation.threebranch.inp", "./PeleC2d.gnu.ex")
