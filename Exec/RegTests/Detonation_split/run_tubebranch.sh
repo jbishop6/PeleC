f644152756fa264b2728506d43b0a55ab6cbc44d
Jennifer Bishop <jenniferjbshp@gmail.com>
2026-02-24 18:17:08 -0700

    Update run_tubebranch.sh

--- Exec/RegTests/Detonation_split/run_tubebranch.sh
+++ Exec/RegTests/Detonation_split/run_tubebranch.sh
@@ -22,7 +22,7 @@ OUTDIR="TubeBranch_outputs"
 mkdir -p "$OUTDIR"
 
 # === Run PeleC with two-branch input file ===
-./PeleC2d.gnu.ex input_detonation_tubebranch.inp
+./PeleC2d.gnu.MPI.ex input_detonation_tubebranch.inp
 
 # === Move plotfiles to output folder ===
 mv plt* "$OUTDIR"
