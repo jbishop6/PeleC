169661e1c9edc4c97bdade7f5080ad4639541883
Jennifer Bishop <jenniferjbshp@gmail.com>
2026-03-05 08:39:30 -0700

    Update run_threebranch.sh

--- Exec/RegTests/Detonation2D/run_threebranch.sh
+++ Exec/RegTests/Detonation2D/run_threebranch.sh
@@ -22,7 +22,7 @@ OUTDIR="ThreeBranch_outputs"
 mkdir -p "$OUTDIR"
 
 # === Run PeleC with two-branch input file ===
-./PeleC2d.gnu.ex inputs.detonation.threebranch.inp
+./PeleC2d.gnu.MPI.ex inputs.detonation.threebranch.inp
 
 # === Move plotfiles to output folder ===
 mv plt* "$OUTDIR"
