7b3ca9552919e5a5eae5c0eada198c610af8bcdf
Jennifer Bishop <jenniferjbshp@gmail.com>
2026-03-05 16:02:36 -0700

    Update make_TB_vs_TB3_runs.py

--- Exec/RegTests/Detonation2D/make_TB_vs_TB3_runs.py
+++ Exec/RegTests/Detonation2D/make_TB_vs_TB3_runs.py
@@ -16,7 +16,7 @@ base_dir = "TB_vs_TB3"
 # Filenames in the *current* Detonation2D directory
 INPUT_FILE_2B = "inputs.detonation.twobranch.inp"
 INPUT_FILE_3B = "inputs.detonation.threebranch.inp"
-EXECUTABLE_NAME = "PeleC2d.gnu.ex"   # change if your exe name is different
+EXECUTABLE_NAME = "PeleC2d.gnu.MPI.ex"   # change if your exe name is different
 
 # Any extra static files PeleC needs (add to this list)
 COMMON_FILES = [
